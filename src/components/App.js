686ffe20c91f1d79d0181e960e2b9cdc4fad3b4e
Moses <moseslhamon@gmail.com>
2019-09-07 10:34:09 +1000

    added in menu bar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MenuBar from "./MenuBar";
+
+
+const App = () => {
+  return (
+    <div>
+      <MenuBar />
+      <h1>App Component</h1>
+    </div>
+  );
+};
+
+export default App;
